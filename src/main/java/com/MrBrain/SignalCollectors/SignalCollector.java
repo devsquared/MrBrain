b1bee807894ee6cfd9599601597fa91f39c2b5d2
Devin Ward <devinw1325wc@gmail.com>
2018-10-31 20:48:44 -0500

    javadoc part 2

--- src/main/java/com/MrBrain/SignalCollectors/SignalCollector.java
+++ src/main/java/com/MrBrain/SignalCollectors/SignalCollector.java
@@ -4,7 +4,18 @@ import java.util.List;
 
 import com.MrBrain.NeuronConnection;
 
+/**
+ * Signal collector that combines the input connections of a neuron prior to neuron magic.
+ */
 public interface SignalCollector
 {
+	/**
+	 * Collect the previous layer output.
+	 *
+	 * @param inputConnections
+	 * 			list of input connections
+	 * @return
+	 * 			double value of calculated input
+	 */
 	public double collectPrevLayerOutput(List<NeuronConnection> inputConnections);
 }
